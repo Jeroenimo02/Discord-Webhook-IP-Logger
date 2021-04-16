796e2e56493c33a73eac035f8bffe67b3dc4858a
Jeroenimo02 <35402210+Jeroenimo02@users.noreply.github.com>
2021-04-16 16:27:25 +0200

    Updated index.php

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 //If your Discord.php file is in another folder you need to change the file path!
 include("Discord.php");
 $sendembed = New Discord();
+
+//Executes the function
+$sendembed->Visitor();
 ?>
 
 </body>
